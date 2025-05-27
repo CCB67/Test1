93395dd6db5edb348651e565cf6a07913a5b54f3
Christopher <Carl-Christopher.Biebow@wiss-edu.ch>
2025-05-27 11:05:29 +0200

    sc3

--- src/main/java/test1/Test.java
+++ src/main/java/test1/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-
+		System.out.println("Hello user!");
+		System.out.println("How are you today?");
 	}
 
 }
